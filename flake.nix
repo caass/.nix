f9c11d7d953ab35787110cb0ee6a1500bbf22e53
Cass Fridkin <cass@swag.lgbt>
2024-12-04 13:50:15 -0700

    Split out configuration from flake

--- flake.nix
+++ flake.nix
@@ -20,45 +20,16 @@
       lix-module,
       ...
     }:
-    let
-      configuration =
-        { pkgs, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = [
-            pkgs.vim
-
-            # Nix LSP / Formatting / Docs
-            pkgs.nixd
-            pkgs.nil
-            pkgs.nixfmt-rfc-style
-            pkgs.nixdoc
-          ];
-
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
-
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 5;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        };
-    in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#side-project-generation-machine
       darwinConfigurations."side-project-generation-machine" = nix-darwin.lib.darwinSystem {
         modules = [
-          configuration
+          {
+            # Set Git commit hash for darwin-version.
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+          }
+          ./machines/macbook/configuration.nix
           lix-module.nixosModules.default
         ];
       };
