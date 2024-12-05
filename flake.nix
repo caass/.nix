906d7663a8957eac35a764705ea83a40d33a6099
Cass Fridkin <cass@swag.lgbt>
2024-12-04 20:03:26 -0700

    Revert back to just the flake.nix

--- flake.nix
+++ flake.nix
@@ -11,10 +11,6 @@
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
@@ -22,26 +18,47 @@
       self,
       nix-darwin,
       lix-module,
-      home-manager,
       ...
     }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.vim
+
+            # Nix LSP / Formatting / Docs
+            pkgs.nixd
+            pkgs.nil
+            pkgs.nixfmt-rfc-style
+            pkgs.nixdoc
+          ];
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#side-project-generation-machine
       darwinConfigurations."side-project-generation-machine" = nix-darwin.lib.darwinSystem {
         modules = [
-          {
-            # Set Git commit hash for darwin-version.
-            system.configurationRevision = self.rev or self.dirtyRev or null;
-          }
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.cass = import ./users/cass.nix;
-          }
-          home-manager.darwinModules.home-manager
-          ./machines/macbook.nix
-          ./roles/base.nix
+          configuration
           lix-module.nixosModules.default
         ];
       };
--- machines/macbook.nix
+++
@@ -1,9 +0,0 @@
-_: {
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 5;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
-
-}
